aa57b75b673a3d23a5b9c8a9ef4ce26694c69c8b
calebflores <calebflores1388@gmail.com>
2024-01-29 13:51:57 -0600

    p6

--- prac.js
+++ prac.js
@@ -194,4 +194,19 @@ MinStack.prototype.getMin = function () {
  * obj.pop()
  * var param_3 = obj.top()
  * var param_4 = obj.getMin()
+ *
+ *
+
  */
+
+
+const coinChange = (coins, amount) => {
+    const dp = Array(amount + 1).fill(Infinity); // This arr tells us how many coins we need for each amount.
+    dp[0] = 0; // To make 0, we need 0 coins.
+    for (let coin of coins) { // Check each coin
+        for (let i = coin; i <= amount; i++) { // Iterate through the entire amount from coin
+            dp[i] = Math.min(dp[i], dp[i - coin] + 1); // Update minimum number of needed coins.
+        }
+    }
+    return dp[amount] === Infinity ? -1 : dp[amount]; // If the last element is Infinity, then we cannot make the amount.
+};
