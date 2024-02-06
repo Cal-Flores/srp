177487116015040c04acab0cffcbaf158a257ddf
calebflores <calebflores1388@gmail.com>
2024-02-06 14:22:18 -0600

    permentation solu 2

--- prac.js
+++ prac.js
@@ -493,3 +493,19 @@ var merge = function (intervals) {
 
     return merged;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function (nums) {
+    let max = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        max = Math.max(nums[i] + i, max);
+        if (max >= nums.length - 1) return true;
+        if (max == i && nums[i] == 0) return false;
+    }
+
+    return false;
+};
