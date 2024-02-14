633afa05a57c8ad05837bd59501bc433987d246f
calebflores <calebflores1388@gmail.com>
2024-02-14 15:07:55 -0600

    node fix

--- prac.js
+++ prac.js
@@ -562,55 +562,33 @@ class LRUCache {
 }
 /**
  * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
  * }
  */
-
-function insertionSortList(head: ListNode | null): ListNode | null {
-    if (!head) return null
-    if (!head.next) return head
-
-    let output = head
-    let curr = head.next
-
-    head.next = null
-
-    while (curr) {
-        const next = curr.next
-        const insertion = curr
-
-        output = insert(output, insertion)
-        curr = next as ListNode
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var insertionSortList = function (head) {
+    //declare empty array
+    let arr = [];
+    while (head) {
+        //loop : push all list value in array
+        arr.push(head.val)
+        head = head.next
     }
+    //sort the array
+    arr.sort((a, b) => a - b)
 
-    return output
-}
-
-function insert(head: ListNode, other: ListNode) {
-    let curr = head
-    const val = other.val
-
-    if (val <= head.val) {
-        other.next = head
-        return other
+    let res = new ListNode(arr[0])
+    let result = res
+    for (let i = 1; i < arr.length; i++) {
+        // now sorted array value in list
+        result.next = new ListNode(arr[i])
+        result = result.next
     }
-
-    while (curr) {
-        if ((val > curr.val && curr.next && val <= curr.next.val) || !curr.next) {
-            other.next = curr.next
-            curr.next = other
-
-            return head
-        }
-
-        curr = curr.next as ListNode
-    }
-
-    return head
-}
+    //return the final solution
+    return res
+};
