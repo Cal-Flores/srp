64e9dd51e1e4c8e2db095b1913dabab5f12cd12c
calebflores <calebflores1388@gmail.com>
2024-02-23 14:06:06 -0600

    long consecutive

--- prac.js
+++ prac.js
@@ -764,3 +764,26 @@ function ladderLength(beginWord, endWord, wordList) {
 
     return 0;
 }
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function (nums) {
+    if (nums.length == 0) return 0;
+    let s = new Set()
+    let res = 1
+    for (let i = 0; i < nums.length; i++) {
+        s.add(nums[i])
+    }
+    for (const value of s.values()) {
+        if (!s.has(value - 1) && s.has(value + 1)) {
+            let k = 1;
+            while (s.has(value + k)) { k++ }
+            res = Math.max(res, k)
+        }
+    }
+    return res
+
+};
