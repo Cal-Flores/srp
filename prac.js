c052e5f3749081a7975247a38eb72c1194935364
calebflores <calebflores1388@gmail.com>
2024-02-28 13:48:06 -0600

    arrray shuffle

--- prac.js
+++ prac.js
@@ -1140,3 +1140,24 @@ var canConstruct = function (ransomNote, magazine) {
     if (!ransomNote) return true;
     else return false;
 };
+
+var Solution = function (nums) {
+    this.nums = [...nums];
+    this.arr = nums;
+};
+Solution.prototype.reset = function () {
+    this.arr = [...this.nums];
+    return this.arr;
+};
+Solution.prototype.shuffle = function () {
+    let shuffle = (arr) => {
+        for (let i = 0; i < arr.length; i++) {
+            let rand = Math.floor(Math.random() * arr.length);
+            let temp = arr[i];
+            arr[i] = arr[rand];
+            arr[rand] = temp;
+        }
+    }
+    shuffle(this.arr);
+    return this.arr;
+};
