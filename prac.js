152a2fa788c6ef745be8e1cd15e9ab3d54bb7405
calebflores <calebflores1388@gmail.com>
2024-02-28 13:47:35 -0600

    construct revisit

--- prac.js
+++ prac.js
@@ -1132,4 +1132,11 @@ const mergeKLists = function (lists) {
 
     return head.next
 }
+var canConstruct = function (ransomNote, magazine) {
+    for (const char of magazine) {
+        ransomNote = ransomNote.replace(char, "");
+    }
 
+    if (!ransomNote) return true;
+    else return false;
+};
