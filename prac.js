7db8b1fcd52aa5c6f3eb4d770c558e0067c5fecb
calebflores <calebflores1388@gmail.com>
2024-02-28 13:49:05 -0600

    climb stairs

--- prac.js
+++ prac.js
@@ -1161,3 +1161,20 @@ Solution.prototype.shuffle = function () {
     shuffle(this.arr);
     return this.arr;
 };
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+    let prv1 = 1;
+    let prv2 = 1;
+
+    for (let i = 0; i < n; i++) {
+        let tmp = prv1;
+        prv1 = prv1 + prv2;
+        prv2 = tmp;
+    }
+
+    return prv2;
+};
