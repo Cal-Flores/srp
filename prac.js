bc142f3d53639ddc687ed812f2aeab7bf0208c97
calebflores <calebflores1388@gmail.com>
2024-02-29 12:49:50 -0600

    climb stairs fixed

--- prac.js
+++ prac.js
@@ -1178,3 +1178,20 @@ var climbStairs = function (n) {
 
     return prv2;
 };
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+    let prv1 = 1;
+    let prv2 = 1;
+
+    for (let i = 0; i < n; i++) {
+        let tmp = prv1;
+        prv1 = prv1 + prv2;
+        prv2 = tmp;
+    }
+
+    return prv2;
+};
