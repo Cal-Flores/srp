4239a7c427d15780b7eca419e6fc3b979bb02957
calebflores <calebflores1388@gmail.com>
2024-03-02 23:29:59 -0600

    number of digit one

--- prac.js
+++ prac.js
@@ -1252,3 +1252,12 @@ MyQueue.prototype.empty = function () {
  * var param_3 = obj.peek()
  * var param_4 = obj.empty()
  */
+
+
+var countDigitOne = function (n) {
+    if (n <= 0) return 0;
+    if (n < 10) return 1;
+    var base = Math.pow(10, n.toString().length - 1);
+    var answer = parseInt(n / base);
+    return countDigitOne(base - 1) * answer + (answer === 1 ? (n - base + 1) : base) + countDigitOne(n % base);
+};
