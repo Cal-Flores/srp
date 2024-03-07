b82dcfc3b364d71df18062280689804caf831706
calebflores <calebflores1388@gmail.com>
2024-03-07 11:27:20 -0600

    time comp

--- prac.js
+++ prac.js
@@ -1372,3 +1372,23 @@ const kthSmallest = function (matrix, k) {
 
     return q.front().element;
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var combinationSum4 = function (nums, target) {
+    const dp = Array(target + 1).fill(0);
+    dp[0] = 1;
+
+    for (let i = 1; i <= target; i++) {
+        for (const num of nums) {
+            if (i - num >= 0) {
+                dp[i] += dp[i - num];
+            }
+        }
+    }
+
+    return dp[target];
+};
