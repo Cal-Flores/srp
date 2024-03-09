0f0bb0bcf8055cb583e3bf3f66e868d9981b0e21
calebflores <calebflores1388@gmail.com>
2024-03-09 10:11:36 -0600

    time comp

--- prac.js
+++ prac.js
@@ -1391,4 +1391,37 @@ var combinationSum4 = function (nums, target) {
     }
 
     return dp[target];
+}; \
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+    if (!head || !head.next) {
+        return false;
+    }
+
+    let slow = head;
+    let fast = head;
+
+    while (fast.next && fast.next.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+
+        if (slow === fast) {
+            return true;
+        }
+    }
+
+    return false;
 };
