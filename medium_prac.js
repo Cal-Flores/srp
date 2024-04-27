854821c99288ec2c1ff4f26216be4f7cbe0296c7
calebflores <calebflores1388@gmail.com>
2024-04-27 09:56:34 -0500

    medium js

--- medium_prac.js
+++ medium_prac.js
@@ -491,11 +491,4 @@ graph.addEdge("E", "F", 1);
 console.log(graph.dijkstra("A", "E")); // Output: [ 'A', 'C', 'D', 'F', 'E' ]
 
 
-def twoSum(nums, target):
-num_map = {}
-for i, num in enumerate(nums):
-    complement = target - num
-if complement in num_map:
-    return [num_map[complement], i]
-num_map[num] = i
-return None
+
