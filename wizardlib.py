d479c9ffd366e7e5813a4b62d9cb4a1ccfc0ddaa
calebflores <calebflores1388@gmail.com>
2024-05-10 18:47:03 -0500

    castler

--- wizardlib.py
+++ wizardlib.py
@@ -142,3 +142,38 @@ _valid_colors = [
     "lightgray",
     "gainsboro",
 ]
+
+def _is_valid_element(func_name):
+    """
+    Allows us to throw a helpful error message if someone passes a raw value
+    to a function that accepts a DOM element. For some reason, Transcrypt
+    wants this definition before it's used, so it has to be at the top of the
+    file.
+    """
+
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            element = args[0]
+            if isinstance(element, (float, int, str)):
+                raise Exception(
+                    f"Error in {func_name}(), invalid element passed as first "
+                    " argument\n"
+                    f"  - '{element}' is not a valid element!\n"
+                    f"  - Did you pass an element created with add_image(), "
+                    "add_text(), or add_button() as the first argument?\n"
+                )
+            # This will catch functions passed in where an element is exepected
+            # or `undefined` JS vars that sneak through.
+            elif callable(element) or not element:
+                raise Exception(
+                    f"Error in {func_name}(), invalid element passed as first"
+                    " argument\n"
+                    f"  - Did you pass an element created with add_image(), "
+                    "add_text(), or add_button() as the first argument?\n"
+                )
+
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
