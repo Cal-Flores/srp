710f187c7c3a74ae2094a896b5561e0e645db30a
calebflores <calebflores1388@gmail.com>
2024-06-08 13:46:25 -0500

    draw board

--- tiktactoe.py
+++ tiktactoe.py
@@ -1,1 +1,10 @@
 import random
+
+def draw_board():
+    for row in range(0,3):
+        for row_item in range(0,3):
+            symbol = board[row][row_item]
+            if symbol != 0:
+                mark = symbols[symbol]
+                symbol_text = add_text(mark, 40)
+                position_element(symbol_text, 260 + row_item * 130, 250 + row * 130)
