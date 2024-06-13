d9273cf4ed0e7ac4c03b9f020464c50a8c3ccdff
calebflores <calebflores1388@gmail.com>
2024-06-13 14:04:41 -0500

    wrestling obj

--- wrestling.js
+++ wrestling.js
@@ -1,77 +1,320 @@
 const d1WrestlingSchools = [
-    "Air Force Academy",
-    "American University",
-    "Appalachian State University",
-    "Arizona State University",
-    "Binghamton University",
-    "Bloomsburg University of Pennsylvania",
-    "Boise State University",
-    "Boston University",
-    "Brown University",
-    "Bucknell University",
-    "California Polytechnic State University",
-    "Campbell University",
-    "Central Michigan University",
-    "The Citadel",
-    "Clarion University of Pennsylvania",
-    "Cleveland State University",
-    "Columbia University",
-    "Cornell University",
-    "Davidson College",
-    "Drexel University",
-    "Duke University",
-    "East Tennessee State University",
-    "Eastern Michigan University",
-    "Edinboro University of Pennsylvania",
-    "Fairleigh Dickinson University",
-    "George Mason University",
-    "George Washington University",
-    "Harvard University",
-    "Hofstra University",
-    "University of Illinois at Urbana-Champaign",
-    "Indiana University Bloomington",
-    "University of Iowa",
-    "Iowa State University",
-    "University of Maryland, College Park",
-    "University of Michigan",
-    "Michigan State University",
-    "University of Minnesota",
-    "Missouri University of Science and Technology",
-    "University of Missouri",
-    "North Carolina State University",
-    "University of North Carolina at Chapel Hill",
-    "North Dakota State University",
-    "Northern Colorado",
-    "Northern Illinois University",
-    "Northwestern University",
-    "The Ohio State University",
-    "Old Dominion University",
-    "University of Oklahoma",
-    "Oklahoma State University",
-    "Oregon State University",
-    "University of Pennsylvania",
-    "Pennsylvania State University",
-    "Princeton University",
-    "Purdue University",
-    "Rider University",
-    "Rutgers, The State University of New Jersey",
-    "Sacred Heart University",
-    "University of South Dakota",
-    "South Dakota State University",
-    "Southern Illinois University Edwardsville",
-    "Stanford University",
-    "University at Buffalo, The State University of New York",
-    "University of Tennessee at Chattanooga",
-    "University of Tennessee at Martin",
-    "Utah Valley University",
-    "Virginia Military Institute",
-    "Virginia Tech",
-    "VMI Military Institute",
-    "VMI Military Institute",
-    "West Virginia University",
-    "University of Wisconsin-Madison",
-    "University of Wisconsin-Milwaukee",
-    "Wyoming"
+    {
+        name: "American University",
+        conference: "Patriot League"
+    },
+    {
+        name: "Appalachian State University",
+        conference: "Sun Belt Conference"
+    },
+    {
+        name: "Arizona State University",
+        conference: "Pacific-12 Conference"
+    },
+    {
+        name: "Bellarmine University",
+        conference: "ASUN Conference"
+    },
+    {
+        name: "Brown University",
+        conference: "Ivy League"
+    },
+    {
+        name: "Bucknell University",
+        conference: "Patriot League"
+    },
+    {
+        name: "California Baptist University",
+        conference: "Western Athletic Conference"
+    },
+    {
+        name: "California Polytechnic State University – San Luis Obispo",
+        conference: "Big Sky Conference"
+    },
+    {
+        name: "California State University – Bakersfield",
+        conference: "Big West Conference"
+    },
+    {
+        name: "Campbell University",
+        conference: "Big South Conference"
+    },
+    {
+        name: "Central Michigan University",
+        conference: "Mid-American Conference"
+    },
+    {
+        name: "Clarion University of Pennsylvania",
+        conference: "Mid-American Conference (MAC)"
+    },
+    {
+        name: "Cleveland State University",
+        conference: "Horizon League"
+    },
+    {
+        name: "Columbia University",
+        conference: "Ivy League"
+    },
+    {
+        name: "Cornell University",
+        conference: "Ivy League"
+    },
+    {
+        name: "Davidson College",
+        conference: "Atlantic 10 conference"
+    },
+    {
+        name: "Drexel University",
+        conference: "Colonial Athletic Association"
+    },
+    {
+        name: "Duke University",
+        conference: "Atlantic Coast Conference"
+    },
+    {
+        name: "Franklin & Marshall College",
+        conference: "Eastern Intercollegiate Wrestling Association"
+    },
+    {
+        name: "Gardner-Webb University",
+        conference: "Big South Conference"
+    },
+    {
+        name: "George Mason University",
+        conference: "Atlantic 10 Conference"
+    },
+    {
+        name: "Harvard University",
+        conference: "Ivy League"
+    },
+    {
+        name: "Hofstra University",
+        conference: "Colonial Athletic Association"
+    },
+    {
+        name: "Indiana University",
+        conference: "Big Ten Conference"
+    },
+    {
+        name: "Iowa State University",
+        conference: "Big 12 Conference"
+    },
+    {
+        name: "Kent State University",
+        conference: "Mid-American Conference"
+    },
+    {
+        name: "Lehigh University",
+        conference: "Patriot League"
+    },
+    {
+        name: "Lindenwood University",
+        conference: "Ohio Valley Conference"
+    },
+    {
+        name: "Lock Haven University",
+        conference: "Atlantic 10 Conference"
+    },
+    {
+        name: "Long Island University",
+        conference: "Northeast Conference"
+    },
+    {
+        name: "Michigan State University",
+        conference: "Big Ten Conference"
+    },
+    {
+        name: "Morgan State University",
+        conference: "Mid-Eastern Athletic Conference"
+    },
+    {
+        name: "North Carolina State University",
+        conference: "Atlantic Coast Conference"
+    },
+    {
+        name: "North Dakota State University",
+        conference: "Big 12 Conference"
+    },
+    {
+        name: "Northern Illinois University",
+        conference: "Mid-American Conference"
+    },
+    {
+        name: "Northwestern University",
+        conference: "Big Ten Conference"
+    },
+    {
+        name: "The Ohio State University",
+        conference: "Big Ten Conference"
+    },
+    {
+        name: "Ohio University",
+        conference: "Mid-American Conference"
+    },
+    {
+        name: "Oklahoma State University",
+        conference: "Big 12 Conference"
+    },
+    {
+        name: "Oregon State University",
+        conference: "Pacific-12 Conference"
+    },
+    {
+        name: "Pennsylvania State University",
+        conference: "Big Ten Conference"
+    },
+    {
+        name: "Presbyterian College",
+        conference: "Big South Conference"
+    },
+    {
+        name: "Princeton University",
+        conference: "Ivy League"
+    },
+    {
+        name: "Purdue University",
+        conference: "Big Ten Conference"
+    },
+    {
+        name: "Queens University of Charlotte",
+        conference: "Atlantic Sun Conference"
+    },
+    {
+        name: "Rider University",
+        conference: "Metro Atlantic Athletic Conference"
+    },
+    {
+        name: "Rutgers University",
+        conference: "Big Ten Conference"
+    },
+    {
+        name: "Sacred Heart University",
+        conference: "Northeast Conference"
+    },
+    {
+        name: "South Dakota State University",
+        conference: "Summit League Conference"
+    },
+    {
+        name: "Southern Illinois University Edwardsville",
+        conference: "Ohio Valley Conference"
+    },
+    {
+        name: "Stanford University",
+        conference: "Pacific-12 Conference"
+    },
+    {
+        name: "SUNY Binghamton University",
+        conference: "America East Conference"
+    },
+    {
+        name: "SUNY University at Buffalo",
+        conference: "Mid-American Conference"
+    },
+    {
+        name: "The Citadel",
+        conference: "Southern Conference"
+    },
+    {
+        name: "United States Air Force Academy",
+        conference: "Mountain West Conference"
+    },
+    {
+        name: "United States Military Academy",
+        conference: "Patriot League"
+    },
+    {
+        name: "United States Naval Academy",
+        conference: "Patriot League"
+    },
+    {
+        name: "University of Arkansas at Little Rock",
+        conference: "Sun Belt Conference"
+    },
+    {
+        name: "University of Illinois",
+        conference: "Big Ten Conference"
+    },
+    {
+        name: "University of Iowa",
+        conference: "Big Ten Conference"
+    },
+    {
+        name: "University of Maryland",
+        conference: "Big Ten Conference"
+    },
+    {
+        name: "University of Michigan",
+        conference: "Big Ten Conference"
+    },
+    {
+        name: "University of Minnesota",
+        conference: "Big Ten Conference"
+    },
+    {
+        name: "University of Missouri",
+        conference: "Southeastern Conference"
+    },
+    {
+        name: "University of Nebraska",
+        conference: "Big Ten Conference"
+    },
+    {
+        name: "University of North Carolina at Chapel Hill",
+        conference: "Atlantic Coast Conference"
+    },
+    {
+        name: "University of Northern Colorado",
+        conference: "Big Sky Conference"
+    },
+    {
+        name: "University of Northern Iowa",
+        conference: "Missouri Valley Conference"
+    },
+    {
+        name: "University of Oklahoma",
+        conference: "Big 12 Conference"
+    },
+    {
+        name: "University of Pennsylvania – Penn",
+        conference: "Ivy League"
+    },
+    {
+        name: "University of Pittsburgh",
+        conference: "Atlantic Coast Conference"
+    },
+    {
+        name: "University of Tennessee – Chattanooga",
+        conference: "Southern Conference"
+    },
+    {
+        name: "University of Virginia",
+        conference: "Atlantic Coast Conference"
+    },
+    {
+        name: "University of Wisconsin",
+        conference: "Big Ten Conference"
+    },
+    {
+        name: "University of Wyoming",
+        conference: "Mountain West Conference"
+    },
+    {
+        name: "Utah Valley University",
+        conference: "Western Athletic Conference"
+    },
+    {
+        name: "Virginia Military Institute – VMI",
+        conference: "Big South Conference"
+    },
+    {
+        name: "Virginia Tech",
+        conference: "Atlantic Coast Conference"
+    },
+    {
+        name: "West Virginia University",
+        conference: "Big 12 Conference"
+    }
 ];
 
-console.log(d1WrestlingSchools);
+console.log(d1WrestlingSchools.length);
