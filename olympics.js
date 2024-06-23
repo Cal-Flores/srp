be8711ea073dbacff8f670d7780350c2a1877197
calebflores <calebflores1388@gmail.com>
2024-06-22 21:19:47 -0500

    fl7

--- olympics.js
+++ olympics.js
@@ -2,3 +2,6 @@ flyWeight = [
     'Alexandre Pantoja', 'Stephan Micic', 'Spencer Lee',
     'Manel Kape', 'Tagir Ulanbekov', 'Tatsumitsu Wada'
 ]
+
+
+let fly = ['Brandon moreno', 'kai kara france', 'devidson figurdo', 'alex panoja', 'demetrious johnson']
